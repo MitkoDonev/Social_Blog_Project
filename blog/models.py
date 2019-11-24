6b2db6b603dd64c120265712c1bb6dbb72a8ce12
MDonev <donev90@gmail.com>
2019-11-24 20:45:50 +0200

    Update

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 # it will hold the user model and the blog post model
-from blog import db, login_manager
+from app import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
